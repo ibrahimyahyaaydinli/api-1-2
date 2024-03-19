90e2c37aa8933849129d29bc4cab59b20ee2e2d1
Yahya Aydinli <ibrahimyahyaaydinli@gmail.com>
2024-03-19 08:44:40 +0300

    vercel.json is added v4

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get("/url", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.listen("http://drwwrd.site", () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
